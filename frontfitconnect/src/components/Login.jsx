54a9f64c6abb03c4d7a498e1aea6e9d25b702ca7
Foued <fouad.kabaou@gmail.com>
2024-01-12 21:21:24 +0100

    login Page

--- frontfitconnect/src/components/Login.jsx
+++ frontfitconnect/src/components/Login.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/Login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("athlete");
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username :</label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password :</label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="role">Role :</label>
+          <select
+            name="role"
+            id="role"
+            onChange={(e) => setRole(e.target.value)}
+          >
+            <option value="coach">Coach</option>
+            <option value="athlete">Athlete</option>
+          </select>
+        </div>
+        <button className="btn-login" onClick={handleSubmit}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
